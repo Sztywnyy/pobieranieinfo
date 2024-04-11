d93a964d328aef556b86ee7213698f765f4da05e
Sztywnyy <122525496+Sztywnyy@users.noreply.github.com>
2024-04-11 15:55:06 +0200

    Add files via upload

--- fetchData.php
+++ fetchData.php
@@ -1,30 +1,39 @@
 <?php
-header('Access-Control-Allow-Origin: *'); // Zezwól na żądania z dowolnego źródła
-header('Content-Type: application/json; charset=utf-8'); // Ustawienie nagłówka Content-Type na JSON
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
 
-// Dane dostępowe do bazy danych
-$host = 'localhost';
-$dbname = 'praktyki';
-$username = 'root';
-$password = '';
+class Database {
+    private $host = 'localhost';
+    private $dbname = 'praktyki';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=utf8", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Błąd połączenia: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
 
 try {
-    // Utworzenie połączenia z bazą danych przy użyciu PDO
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // Ustawienie atrybutu, aby wyjątki były rzucane w przypadku błędu
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $database = new Database();
+    $pdo = $database->getConnection();
 
-    // Zapytanie SQL do pobrania wszystkich postów
     $sql = "SELECT id, title, content FROM post";
     $stmt = $pdo->query($sql);
 
-    // Pobranie wyników zapytania
-    $post = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Zwrócenie wyników w formacie JSON
-    echo json_encode($post);
+    echo json_encode($posts);
 } catch (PDOException $e) {
-    // Zwrócenie błędu, jeśli coś pójdzie nie tak
     http_response_code(500);
     echo json_encode(['error' => $e->getMessage()]);
 }

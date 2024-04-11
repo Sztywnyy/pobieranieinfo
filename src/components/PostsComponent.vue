cc97e014f7f6dbe36cf787ea3656ef4edd18cb2d
Sztywnyy <dawidkowalczyk0425@gmail.com>
2024-04-11 04:12:32 +0200

    styl bledu

--- src/components/PostsComponent.vue
+++ src/components/PostsComponent.vue
@@ -1,40 +1,57 @@
 <template>
-    <div>
-      <h2>Posty</h2>
-      <div v-if="posts.length">
-        <div class="post-container" v-for="post in posts" :key="post.id">
-          <h3>{{ post.title }}</h3>
-          <p>{{ post.content }}</p>
-        </div>
+  <div>
+    <h2>Posty</h2>
+    <div v-if="posts.length">
+      <div class="post-container" v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.content }}</p>
       </div>
-      <p v-else>Brak postów do wyświetlenia.</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        posts: []
-      };
+    <p class="blad" v-else>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="currentColor"
+        class="bi bi-question-octagon"
+        viewBox="0 0 16 16"
+        style="margin-right: 8px;"
+      >
+        <path
+          d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1z"
+        />
+        <path
+          d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94"
+        /></svg
+      >Brak postów do wyświetlenia.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  methods: {
+    fetchPosts() {
+      fetch("http://localhost/skrypty/fetchdata.php")
+        .then((response) => response.json())
+        .then((data) => {
+          this.posts = data;
+        })
+        .catch((error) => {
+          console.error("Wystąpił błąd podczas pobierania postów: ", error);
+        });
     },
-    mounted() {
-      this.fetchPosts();
-    },
-    methods: {
-      fetchPosts() {
-        fetch('http://localhost/skrypty/fetchdata.php')
-          .then(response => response.json())
-          .then(data => {
-            this.posts = data;
-          })
-          .catch(error => {
-            console.error("Wystąpił błąd podczas pobierania postów: ", error);
-          });
-      }
-    }
-  }
-  </script>
+  },
+};
+</script>
 
 <style scoped>
 * {
@@ -46,7 +63,14 @@ body {
 }
 
 h2 {
-  font-size: 50px;
+  color: #000000;
+  font-family: "Raleway", sans-serif;
+  font-size: 62px;
+  font-weight: 800;
+  line-height: 72px;
+  margin: 0 0 24px;
+  text-align: center;
+  text-transform: uppercase;
   text-align: center;
 }
 
@@ -56,13 +80,29 @@ h2 {
   padding: 20px;
   background-color: #f0f4f8;
   border-radius: 10px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+}
+
+p.blad {
+  display: flex;
+  justify-content: center;
+  font-weight: 550;
+  max-width: 600px;
+  margin: 20px auto;
+  padding: 20px;
+  background-color: #f0f4f8;
+  color: #cc0000;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
 }
 
 h3 {
   font-weight: bold;
 }
 </style>
-  
